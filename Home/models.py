51e14cdef47f95fa3a19dfb7eadec0d683b37e73
Ashutosh Patil <ashutoshpatil470@gmail.com>
2025-07-21 16:21:22 +0530

    image_fix

--- Home/models.py
+++ Home/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class Sell(models.Model):
     product_name = models.CharField(max_length=100)
     product_age = models.CharField(max_length=100)
     product_discription = models.TextField()
     product_price = models.CharField(max_length=100)
-    contact = models.CharField(max_length=15,default='9999999999')
-    product_file = models.ImageField(upload_to='images/')
+    contact = models.CharField(max_length=15, default='9999999999')
+    product_file = models.ImageField(upload_to='images/')  # Cloudinary will handle this path
 
     def __str__(self):
         return self.product_name
